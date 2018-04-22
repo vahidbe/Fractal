84e3a07f7e9fe34eef6eee09d8b62031c15bf586
vahidbe <vahidbe@users.noreply.github.com>
2018-04-22 15:47:54 +0200

    Init

---
+++ ProjetFractal/libfractal/fractal.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "fractal.h"
+
+struct fractal *fractal_new(const char *name, int width, int height, double a, double b)
+{
+    /* TODO */
+    return NULL;
+}
+
+void fractal_free(struct fractal *f)
+{
+    /* TODO */
+}
+
+const char *fractal_get_name(const struct fractal *f)
+{
+    /* TODO */
+    return NULL;
+}
+
+int fractal_get_value(const struct fractal *f, int x, int y)
+{
+    /* TODO */
+    return 0;
+}
+
+void fractal_set_value(struct fractal *f, int x, int y, int val)
+{
+    /* TODO */
+}
+
+int fractal_get_width(const struct fractal *f)
+{
+    /* TODO */
+    return 0;
+}
+
+int fractal_get_height(const struct fractal *f)
+{
+    /* TODO */
+    return 0;
+}
+
+double fractal_get_a(const struct fractal *f)
+{
+    /* TODO */
+    return 0;
+}
+
+double fractal_get_b(const struct fractal *f)
+{
+    /* TODO */
+    return 0;
+}
