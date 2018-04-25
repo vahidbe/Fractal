4c4d20d1fb1f50dad75af43f4a109328d5602a13
vahidbe <vahidbe@users.noreply.github.com>
2018-04-25 20:59:43 +0200

    -

--- ProjetFractal/main.c
+++ ProjetFractal/main.c
@@ -6,6 +6,6 @@
 int main(int argc, char *argv[])
 {
 	struct fractal* f1 = fractal_new(argv[1],*argv[2]-'0',*argv[3]-'0',*argv[4]-'0',*argv[5]-'0');
-	printf("%s",fractal_get_name(f1);
+	printf("%s",fractal_get_name(f1));
 	fractal_free(f1);
 }
