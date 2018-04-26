a734b6015f5deac452d565a08dae8de7f8aec98f
vahidbe <vahidbe@users.noreply.github.com>
2018-04-26 20:14:11 +0200

    corrections compilation

--- ProjetFractal/main.c
+++ ProjetFractal/main.c
@@ -19,7 +19,12 @@
 //TODO: redemander l'architecture à suivre
 //TODO: gérer le fait que le producteur consommateur utilise des struct fractale* au lieu de int
 
-typedef struct {
+struct args{
+		int argc_arg;
+		char** argv_arg;
+		struct sbuf* buf_arg;
+};	
+struct sbuf{
     int *buf;          /* Buffer partagé */
     int n;             /* Nombre de slots dans le buffer */
     int front;         /* buf[(front+1)%n] est le premier élément */
@@ -27,13 +32,13 @@ typedef struct {
     sem_t mutex;       /* Protège l'accès au buffer */
     sem_t slots;       /* Nombre de places libres */
     sem_t items;       /* Nombre d'items dans le buffer */
-} sbuf_t;
+};
 int entreestd;
 /*
  * @pre sp!=NULL, n>0
  * @post a construit un buffer partagé contenant n slots
  */
-void sbuf_init(sbuf_t *sp, int n)
+void sbuf_init(struct sbuf *sp, int n)
 {
     sp->buf = calloc(n, sizeof(int));
     sp->n = n;                       /* Buffer content les entiers */
@@ -47,7 +52,7 @@ void sbuf_init(sbuf_t *sp, int n)
  * @pre sp!=NULL
  * @post libère le buffer
  */
-void sbuf_clean(sbuf_t *sp)
+void sbuf_clean(struct sbuf *sp)
 {
     free(sp->buf);
 }
@@ -56,7 +61,7 @@ void sbuf_clean(sbuf_t *sp)
  * @post ajoute item à la fin du buffer partagé. Ce buffer est géré
  *       comme une queue FIFO
  */
-void sbuf_insert(sbuf_t *sp, struct fractal* item)
+void sbuf_insert(struct sbuf *sp, struct fractal* item)
 {
 	sem_wait(&(sp->slots));
 	sem_wait(&(sp->mutex));
@@ -68,7 +73,7 @@ void sbuf_insert(sbuf_t *sp, struct fractal* item)
 /* @pre sbuf!=NULL
  * @post retire le dernier item du buffer partagé
  */
-struct fractal* sbuf_remove(sbuf_t *sp)
+struct fractal* sbuf_remove(struct sbuf *sp)
 {
 	sem_wait(&(sp->items));
 	sem_wait(&(sp->mutex));
@@ -80,12 +85,13 @@ struct fractal* sbuf_remove(sbuf_t *sp)
 }
 void *producer(void* arguments){
 	struct args* argument=(struct args*) arguments;
-	char* argv[]=argument->argv_arg;
+	char** argv=argument->argv_arg;
 	int argc=argument->argc_arg;
-	sbuf_t buf=argument->buf_arg;
+	struct sbuf* buf=argument->buf_arg;
 	
 	if(entreestd){
-		for(int i=0;(i<argc-1) % 5;i++){
+		int i;
+		for(i=0;(i<argc-1) % 5;i++){
 			//TODO: gérer le toInt
 			struct fractal* f = fractal_create(argv[0+i*5],toInt(argv[1+i*5]),toInt(argv[2+i*5]),toInt(argv[3+i*5]),toInt(argv[4+i*5]));
 			sbuf_insert(buf,f);
@@ -98,13 +104,15 @@ void *producer(void* arguments){
 
 struct fractal* consumer(void* arguments){
 	struct args* argument=(struct args*) arguments;
-	char* argv[]=argument->argv_arg;
+	char** argv=argument->argv_arg;
 	int argc=argument->argc_arg;
-	sbuf_t buf=argument->buf_arg;
+	struct sbuf* buf=argument->buf_arg;
 	
 	struct fractal* f=sbuf_remove(buf);
-	for(int i=0;i<f->width;i++){
-		for(int j=0;j<f->height;j++){
+	int i;
+	int j;
+	for(i=0;i<f->width;i++){
+		for(j=0;j<f->height;j++){
 			fractal_set_value(f,i,j,fractal_compute_value(f,i,j));
 		}
 	}
@@ -116,23 +124,18 @@ int main(int argc, char *argv[])
 	int numberThreads = 4;
 	entreestd = 1;
 	
-	sbuf_t* buf; 
-	struct args{
-		int argc_arg;
-		char* argv_arg;
-		sbuf_t* buf_arg;
-	}
+	struct sbuf* buf; 
 	struct args* arguments=(struct args*) malloc(sizeof(struct args));
 	arguments->argc_arg=argc;
 	arguments->argv_arg=argv;
 	
-	sbuf_init(buff, (numberThreads));
+	sbuf_init(buf, (numberThreads));
 	arguments->buf_arg=buf;
 	
 
 	pthread_t prod, cons;	
-	pthread_create(&prod, NULL, producer, (void*) arguments);
-	pthread_create(&cons, NULL, consumer, (void*) arguments);
+	pthread_create(&prod, NULL, (void*) &producer, (void*) arguments);
+	pthread_create(&cons, NULL, (void*) &consumer, (void*) arguments);
 	//TODO: créer un autre consommateur pour s'occuper de la sortie
 	
 	//TODO: gérer la destuction des threads
