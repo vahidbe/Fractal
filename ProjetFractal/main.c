afe24a73eabef8914e9fc699b6d86255e59b4c37
vahidbe <vahidbe@users.noreply.github.com>
2018-04-28 18:40:04 +0200

    update main
    ajout lecture producteur

--- ProjetFractal/main.c
+++ ProjetFractal/main.c
@@ -91,16 +91,136 @@ struct fractal* sbuf_remove(struct sbuf *sp)
 	return res;
 }
 
-void *producer(void* arguments){
+void *producer(void* arguments){	
+	int fdi=0;
 	int done = 0;
 	struct args* argument=(struct args*) arguments;
 	char* fileName=argument->charP_arg;
 	struct sbuf* buf=argument->buf_arg;
 	free(args);
-	while(!done){
-		//TODO: faire les open et lecture de fichier et les insert dans buf
-		//TODO: gérer quand lecture terminée
+	if(open(fileName,fdi)<0)
+		exit(-1);
+		//TODO: gérer les erreurs/la sortie
+	char* buf1 = (char*) malloc(sizeof(char));
+	int* buf2 = (int*) malloc(sizeof(int));
+	int* buf3 = (int*) malloc(sizeof(int));
+	double* buf4 = (double*) malloc(sizeof(double));
+	double* buf5 = (double*) malloc(sizeof(double));
+	char bufName[64];
+	if((buff==NULL)|(buff==NULL)|(buff==NULL)|(buff==NULL)|(buff==NULL))
+	{
+		if(close(fdi)!=0)
+			exit(-1);		
+		//TODO: gérer les erreurs/la sortie
+		exit(-1);
+	}	
+	int val=0;
+	size_t x=0;
+	int i;
+	int j;
+	x=read(fdi,buf1,sizeof(char));
+	for(i=0;!done;i++){
+		if(x!=sizeof(char))
+		{
+			if(close(fdi)!=0)
+			{
+				free(buf1);
+				free(buf2);
+				free(buf3);
+				free(buf4);
+				free(buf5);
+				exit(-1);
+			}
+			free(buf1);
+			free(buf2);
+			free(buf3);
+			free(buf4);
+			free(buf5);
+			done=1;
+		}
+		else
+		{
+			bufName[0]=*buf1;
+			j=1;
+			for(x=read(fdi,buf1,sizeof(char));*buf1!=' ');x=read(fdi,buf1,sizeof(char)))
+			{
+				if(x==-1)
+				{
+					//TODO: gérer les erreurs/la sortie
+					if(close(fdi)!=0)
+						exit(-1);
+					free(buf1);
+					free(buf2);
+					free(buf3);
+					free(buf4);
+					free(buf5);
+					exit(-1);
+				}
+				bufName[j]=*buf1;
+				j++;
+			}
+			char name[j];
+			int k;
+			for(k=0;k<j;k++)
+			{
+				name[k]=bufName[k];
+			}
+			x=read(fdi,buf2,sizeof(int));
+			if(x==-1)
+			{
+				//TODO: gérer les erreurs/la sortie
+				if(close(fdi)!=0)
+					exit(-1);
+				free(buf1);
+				free(buf2);
+				free(buf3);
+				free(buf4);
+				free(buf5);
+				exit(-1);
+			}
+			x=read(fdi,buf3,sizeof(int));
+			if(x==-1)
+			{
+				//TODO: gérer les erreurs/la sortie
+				if(close(fdi)!=0)
+					exit(-1);
+				free(buf1);
+				free(buf2);
+				free(buf3);
+				free(buf4);
+				free(buf5);
+				exit(-1);
+			}
+			x=read(fdi,buf4,sizeof(double));
+			if(x==-1)
+			{
+				//TODO: gérer les erreurs/la sortie
+				if(close(fdi)!=0)
+					exit(-1);
+				free(buf1);
+				free(buf2);
+				free(buf3);
+				free(buf4);
+				free(buf5);
+				exit(-1);
+			}
+			x=read(fdi,buf5,sizeof(double));
+			if(x==-1)
+			{
+				//TODO: gérer les erreurs/la sortie
+				if(close(fdi)!=0)
+					exit(-1);
+				free(buf1);
+				free(buf2);
+				free(buf3);
+				free(buf4);
+				free(buf5);
+				exit(-1);
+			}
+			sbuf_insert(buf,fractal_new(name,*buf2,*buf3,*buf4,*buf5));
+		}
 	}
+		//TODO: gérer quand lecture terminée
 	//TODO: exit? thread_exit? buf_free? etc
 }
 
