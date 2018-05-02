8f5987b21a78896f477d648469254df6657c7c03
vahidbe <vahidbe@users.noreply.github.com>
2018-05-02 20:08:23 +0200

    -

--- main.c
+++ main.c
@@ -275,8 +275,8 @@ void *consumer(void* arguments){
 		pthread_mutex_lock(&mutexCons);
 		int ic=0;
 		sem_getvalue(&(bufIn->items),&ic);
-		printf("\n%d\n",ic);
-		printf("%d\n\n",flagB1);
+		printf("%d ",ic);
+		printf("%d\n",flagB1);
 		if(((flagB1)<=0)&(ic==0))
 		{
 			done=1;
