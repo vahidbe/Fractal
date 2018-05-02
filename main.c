d74bfa35b90d2ce87268e1b02c6a03f9877aa6b8
vahidbe <vahidbe@users.noreply.github.com>
2018-05-02 02:15:10 +0200

    -

--- main.c
+++ main.c
@@ -80,7 +80,7 @@ void sbuf_insert(struct sbuf *sp, struct fractal* item)
 	///**/printf("ITEMS = %d sur %d\n",ic,sp->n);
 	sem_wait(&(sp->slots));
 	sem_wait(&(sp->mutex));
-	printf("Fractale to insert : %s\n",fractal_get_name(&(item)));
+	printf("Fractale to insert : %s\n",fractal_get_name((item)));
 	fflush(stdout);
 	sp->rear=((sp->rear)+1)%(sp->n);
 	sp->buf[sp->rear]=*item;	
