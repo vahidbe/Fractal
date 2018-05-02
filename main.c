21c2c94f876a596ea3c38f8f358182c41f19b156
vahidbe <vahidbe@users.noreply.github.com>
2018-05-02 02:59:49 +0200

    try malloc main

--- main.c
+++ main.c
@@ -80,10 +80,13 @@ void sbuf_insert(struct sbuf *sp, struct fractal* item)
 	///**/printf("ITEMS = %d sur %d\n",ic,sp->n);
 	sem_wait(&(sp->slots));
 	sem_wait(&(sp->mutex));
-	printf("Fractale to insert : %s\n",fractal_get_name((item)));
+	struct fractal* newf= malloc(sizeof(struct fractal));
+	*newf=*item;
+	free(item);
+	printf("Fractale to insert : %s\n",fractal_get_name((newf)));
 	fflush(stdout);
 	sp->rear=((sp->rear)+1)%(sp->n);
-	sp->buf[sp->rear]=item;	
+	sp->buf[sp->rear]=newf;	
 	printf("Number : %d\n",sp->rear);
 	fflush(stdout);
 	sem_post(&(sp->mutex));
