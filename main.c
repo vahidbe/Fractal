a1c5681f45cf16f8b7e24d73b1814b4924b43a9c
vahidbe <vahidbe@users.noreply.github.com>
2018-05-03 21:53:20 +0200

    -

--- main.c
+++ main.c
@@ -264,6 +264,7 @@ void *consumer(void* arguments){
 	{
 		pthread_mutex_lock(&gardien);
 		printf("countProd %d, numberProd %d, fractCountC %d, fractCountP %d \n",countProd,numberProd,fractCountC,fractCountP);
+		if(bufOut->front==bufOut->rear){pthread_mutex_unlock(&gardien);}else{
 		if(((countProd==numberProd)&(bufIn->front==bufIn->rear))|((fractCountC==fractCountP)&(fractCountP!=0)))
 		{
 			done=1;			
@@ -284,7 +285,7 @@ void *consumer(void* arguments){
 				}
 			}
 			sbuf_insert(bufOut,f);
-		}
+		}}
 	}
 	countCons++;	
 	pthread_mutex_lock(&gardien);
@@ -301,6 +302,7 @@ void *writer(void* arguments){
 			pthread_mutex_lock(&gardien);
 			sleep(0);
 			printf("countCons %d, numberThreads %d, fractCountW %d, fractCountP %d\n",countCons,numberThreads,fractCountW,fractCountP); fflush(stdout);
+			if(bufOut->front==bufOut->rear){pthread_mutex_unlock(&gardien);}else{
 			if(((countCons==numberThreads)&(bufOut->front==bufOut->rear))|((fractCountW==fractCountP)&(fractCountP!=0)))
 			{
 				countEleves++;
@@ -325,7 +327,7 @@ void *writer(void* arguments){
 				pthread_mutex_unlock(&gardien);	
 				fractal_free(f);
 				sleep(0);
-			}
+			}}
 			
 		}
 		pthread_mutex_lock(&gardien);
