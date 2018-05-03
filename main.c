13a57c494207d7e55f894cb269403afe431a036c
vahidbe <vahidbe@users.noreply.github.com>
2018-05-03 16:09:33 +0200

    sleep 5

--- main.c
+++ main.c
@@ -337,7 +337,7 @@ void *writer(void* arguments){
 			{
 				printf("1 CONS RESTANT\n");
 				fflush(stdout);
-				sleep(2);
+				sleep(5);
 			}
 			printf("countCons : %d, numberThreads : %d\n",countCons, numberThreads);
 			fflush(stdout);
