fd0775f36c432f7f2cb9c3ba05eae4c784f1fffc
vahidbe <vahidbe@users.noreply.github.com>
2018-05-03 19:20:06 +0200

    Revert "corrections"
    This reverts commit 9065fe1a08a7818095da6dfea974a7a8941f28f0.

--- main.c
+++ main.c
@@ -387,11 +387,11 @@ int main(int argc, char *argv[])
 	int count;
 	int optionsCount=0;
 	optionD=0;
-	bufIn(malloc(sizeof(struct sbuf))); 
-	bufOut=(malloc(sizeof(struct sbuf))); 
+	bufIn;
+	bufOut; 
 	highestF=malloc(sizeof(struct fractal));
 	
-	if((bufIn==NULL)|(bufOut==NULL)|(highestF==NULL))
+	if((highestF==NULL))
 	{
 		fprintf(stderr,"Malloc error!\n");
 	}
@@ -421,9 +421,6 @@ int main(int argc, char *argv[])
 	if(numberProd==0)
 	{
 		fprintf(stderr,"You did'nt enter a file!\n");
-		free(bufIn);
-		free(bufOut);
-		free(highestF);
 		exit(-1);
 	}
 	
