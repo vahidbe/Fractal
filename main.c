7c5bc40b6bc32433137785e77b8edbe826e780bf
vahidbe <vahidbe@users.noreply.github.com>
2018-05-03 18:18:49 +0200

    clean

--- main.c
+++ main.c
@@ -161,8 +161,6 @@ void *producer(void* arguments){
     	if(x==EOF)
 	{		
 		pthread_mutex_lock(&gardien);
-		/**/printf("P - lockG\n");
-		/**/fflush(stdout);
 		if(fclose(file)!=0)
 		{				
 			free(buf1);
